b49cc5035fe97b417658e66cb3e3b1018449c4ce
spish-web <nickolas.spisak@gmail.com>
2022-07-01 13:18:32 -0400

    added personal

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <img className="picture" src="/images/first.jpeg" alt="image" />
-      <img className="picture" src="/images/second.jpeg" alt="image" />
-      <img className="picture" src="/images/third.jpeg" alt="image" />
-      <img className="picture" src="/images/fourth.jpeg" alt="image" />
-      <img className="picture" src="/images/fifth.jpeg" alt="image" />
+      <img className="picture" src="/images/first.jpeg" alt="portfolio" />
+      <img className="picture" src="/images/second.jpeg" alt="portfolio" />
+      <img className="picture" src="/images/third.jpeg" alt="portfolio" />
+      <img className="picture" src="/images/fourth.jpeg" alt="portfolio" />
+      <img className="picture" src="/images/fifth.jpeg" alt="portfolio" />
       <div className="container">
         <div className="row mb-3">
           <div className="col mt-3">
-            <img className="insta" src="/images/insta.jpeg" alt="image" />
+            <img className="insta" src="/images/insta.jpeg" alt="insta" />
           </div>
           <div className="col mt-3">
-            <h2>Profile theme created in Highschool</h2>
+            <h4>Personal Profile theme created in Highschool</h4>
           </div>
         </div>
       </div>
