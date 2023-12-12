5a56a48446c4ce2f21faa3a8f43c6586257592f3
nickspisak <nickolas.spisak@gmail.com>
2023-12-12 13:24:39 -0500

    new resume

--- src/Contact.js
+++ src/Contact.js
@@ -20,7 +20,7 @@ const Contact = () => {
             <div className="col">
               <div></div>
             </div>
-          <div className="col" style={{'marginTop': '-140px'}}>
+          <div className="col" style={{'marginTop': '40px'}}>
         
               <img src="/images/keri_resume.png" height={1100} />
             
